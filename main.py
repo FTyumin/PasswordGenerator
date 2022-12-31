cfd55a5e9c19877bfc67bc8a8a3597f8ceba3521
FTyumin <feodor.tjumin28@gmail.com>
2022-12-31 13:27:36 +0200

    Small changes

--- main.py
+++ main.py
@@ -2,27 +2,25 @@ import PySimpleGUI as sg
 import string
 import random
 
+
 def get_random_string(length):
-    
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
-layout = [[sg.Text('Amount of characters:'), sg.Input()],
-          [sg.Button('Ok'), sg.Button('Cancel')],
-          [sg.Text('',key='_TEXT_'),]]
 
-window = sg.Window('Input window').Layout(layout)
+layout = [[sg.Text('Amount of characters:'), sg.Input()],
+          [sg.Button('Generate'), sg.Button('Exit')],
+          [sg.Output(size=(30,3), key='-OUTPUT-',font=('Helvetica 20')),]]
+
+window = sg.Window('Password generator').Layout(layout)
 
 while True:
     event, values = window.Read()
     if event in (None, 'Cancel'):
         break
     output = values[0]
-    input =get_random_string(int(output))
-    window['_TEXT_'].Update(input)
+    input = get_random_string(int(output))
+    window['-OUTPUT-'].update(input)
 
 window.Close()
-
-
-
