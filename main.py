1357ed188936473bd34d37e0383ba8fc272f2c5a
FTyumin <feodor.tjumin28@gmail.com>
2023-01-01 16:10:54 +0200

    Improved input and design

--- main.py
+++ main.py
@@ -4,23 +4,37 @@ import random
 
 
 def get_random_string(length):
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
+    if length=='':
+        return "Enter a number"
+    else:
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(length))
+        return result_str
 
 
-layout = [[sg.Text('Amount of characters:'), sg.Input()],
+
+layout = [
+          [sg.Text('Length:',font=('Helvetica 15')), sg.Input()],
+          [sg.Checkbox('Add digits')],
           [sg.Button('Generate'), sg.Button('Exit')],
-          [sg.Output(size=(30,3), key='-OUTPUT-',font=('Helvetica 20')),]]
+          [sg.Text(size=(30,3), key='-OUTPUT-',font=('Helvetica 20'))],
+         ]
 
-window = sg.Window('Password generator').Layout(layout)
+window = sg.Window('Password generator',element_justification='c').Layout(layout)
 
 while True:
     event, values = window.Read()
-    if event in (None, 'Cancel'):
+    if event in (None, 'Exit'):
         break
+
     output = values[0]
-    input = get_random_string(int(output))
-    window['-OUTPUT-'].update(input)
+    if output == '':
+        window['-OUTPUT-'].update("Type a number")
+    elif not output.isdigit():
+        window['-OUTPUT-'].update("Type a number")
+    else:
+        input = get_random_string(int(output))
+        window['-OUTPUT-'].update(input)
+
 
 window.Close()
