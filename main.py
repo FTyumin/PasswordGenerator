d50a21cdd8e4565bc438ecc337f8bfc15aa65620
FTyumin <feodor.tjumin28@gmail.com>
2023-01-04 17:43:21 +0200

    Finished the 'copy to clipboard' button

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 import string
 import secrets
+import pyperclip
 
 
 def generate_password(pwd_length, option=0):
@@ -36,7 +37,7 @@ layout = [
     [sg.Checkbox('Add symbols', key='char', default=True)],
     [sg.Button('Generate'), sg.Button('Exit')],
     [sg.Text(size=(30, 3), key='-OUTPUT-', font='Helvetica 20')],
-    [sg.Button('Copy to clipboard')]
+    [sg.Button('Copy to Clipboard', visible=False, key='copy_button')]
 ]
 
 window = sg.Window('Password generator', element_justification='c').Layout(layout)
@@ -54,18 +55,23 @@ while True:
         window['-OUTPUT-'].update("Please enter a valid integer")
         continue
 
-    if not values['digit'] and not values['char']:  # letters only
-        password = generate_password(output, 3)
-    elif values['digit'] and values['char']:
-        password = generate_password(output)
-    elif ['digit'] and not values['char']:
-        password = generate_password(output, 1)
-    elif ['char'] and not values['digit']:
-        password = generate_password(output, 2)
+    if event == 'Generate':
+        if not values['digit'] and not values['char']:  # letters only
+            password = generate_password(output, 3)
+        elif values['digit'] and values['char']:
+            password = generate_password(output)
+        elif ['digit'] and not values['char']:
+            password = generate_password(output, 1)
+        elif ['char'] and not values['digit']:
+            password = generate_password(output, 2)
 
     window['-OUTPUT-'].update(password)
 
-    if event == 'Copy to clipboard':  # copying password to clipboard
-        sg.copy_to_clipboard(password)
+    # updating the layout to include copy button
+    window['copy_button'].update(visible=True)
+
+    # if clipboard button is pressed, password is copied
+    if event == 'copy_button':
+        pyperclip.copy(password)
 
 window.Close()
