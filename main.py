05c868f827d30b719dbc5ad754c307cf6802b361
FTyumin <feodor.tjumin28@gmail.com>
2023-01-05 00:47:31 +0200

    Program displays how strong is the password

--- main.py
+++ main.py
@@ -29,6 +29,40 @@ def generate_password(pwd_length, option=0):
     return pwd
 
 
+# function checks how strong is the password
+def pass_strength(input_string):
+    n = len(input_string)
+
+    # Checking lower alphabet in string
+    hasLower = False
+    hasUpper = False
+    hasDigit = False
+    specialChar = False
+    normalChars = "abcdefghijklmnopqrstu"
+    "vwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 "
+
+    for i in range(n):
+        if input_string[i].islower():
+            hasLower = True
+        if input_string[i].isupper():
+            hasUpper = True
+        if input_string[i].isdigit():
+            hasDigit = True
+        if input_string[i] not in normalChars:
+            specialChar = True
+
+    # Strength of password
+    if (hasLower and hasUpper and
+            hasDigit and specialChar and n >= 8):
+        return "Strong"
+
+    elif ((hasLower or hasUpper) and
+          specialChar and n >= 6):
+        return "Moderate"
+    else:
+        return "Weak"
+
+
 sg.theme('LightGrey5')
 
 layout = [
@@ -37,7 +71,8 @@ layout = [
     [sg.Checkbox('Add symbols', key='char', default=True)],
     [sg.Button('Generate'), sg.Button('Exit')],
     [sg.Text(size=(30, 3), key='-OUTPUT-', font='Helvetica 20')],
-    [sg.Button('Copy to Clipboard', visible=False, key='copy_button')]
+    [sg.Button('Copy to Clipboard', visible=False, key='copy_button')],
+    [sg.Text("Password strength", visible=False, size=(30, 3), key='-OUTPUT2-', font='Helvetica 20')]
 ]
 
 window = sg.Window('Password generator', element_justification='c').Layout(layout)
@@ -64,6 +99,10 @@ while True:
             password = generate_password(output, 1)
         elif ['char'] and not values['digit']:
             password = generate_password(output, 2)
+
+        # when password is generated, strength of the password is shown
+        password_strength = pass_strength(password)
+        window['-OUTPUT2-'].update("Password strength: " + password_strength,visible=True)
 
     window['-OUTPUT-'].update(password)
 
